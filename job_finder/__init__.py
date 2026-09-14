470f8810ecae45cce9ed8113258bec969b68ae8d
bhaveshnivas77392 <bhaveshgithub435@gmail.com>
2026-09-14 13:00:42 +0530

    chore(release): bump version to 1.2.3 (#66)
    Co-authored-by: Sanga Bhavesh Nivas Royal <nanibhavesh@gmail.com>

--- job_finder/__init__.py
+++ job_finder/__init__.py
@@ -7,9 +7,9 @@ try:
     try:
         __version__ = version("job-finder-ai")
     except PackageNotFoundError:
-        __version__ = "1.2.2"
+        __version__ = "1.2.3"
 except ImportError:
-    __version__ = "1.2.2"
+    __version__ = "1.2.3"
 
 __author__ = "Akhil Baja"
 __contributors__ = ["Bhavesh Nivas"]
